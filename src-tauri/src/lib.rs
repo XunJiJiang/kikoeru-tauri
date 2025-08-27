c62cf3d09ae5f9af43f6eb5f723b32f3ee19f1a1
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2025-08-27 16:28:25 +0800

    build: 使用 vite 进行前端文件打包

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,7 @@ fn open_new_window(app: tauri::AppHandle, label: String, url: String) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
--- src/main.js
+++ src/main.js
@@ -1,87 +1,99 @@
-console.log(window.__TAURI__);
+import { Command } from '@tauri-apps/plugin-shell';
+import { invoke } from '@tauri-apps/api/core';
+import { message } from '@tauri-apps/plugin-dialog';
 
-const { Command } = window.__TAURI__.shell;
-const { invoke } = window.__TAURI__.core;
-
-let platform = '';
-if (navigator.userAgentData && navigator.userAgentData.platform) {
-  platform = navigator.userAgentData.platform.toLowerCase();
-} else {
-  platform = navigator.userAgent.toLowerCase();
-}
-
-const openNewWindowButton = document.querySelector('#open-new-window');
-const restartServiceButton = document.querySelector('#restart-service');
-const outputElement = document.querySelector('#output');
-
-openNewWindowButton.disabled = true;
-
-openNewWindowButton.onclick = () => {
-  if (localhostWebsite) {
-    invoke('open_new_window', { label: `main-${id++}`, url: localhostWebsite });
+try {
+  let platform = '';
+  if (navigator.userAgentData && navigator.userAgentData.platform) {
+    platform = navigator.userAgentData.platform.toLowerCase();
+  } else {
+    platform = navigator.userAgent.toLowerCase();
   }
-};
-restartServiceButton.onclick = async () => {
-  (await cmd).kill();
-  cmd = runKikoeruServe();
-  localhostWebsite = '';
+
+  const openNewWindowButton = document.querySelector('#open-new-window');
+  const restartServiceButton = document.querySelector('#restart-service');
+  const outputElement = document.querySelector('#output');
+
   openNewWindowButton.disabled = true;
-  outputElement.innerHTML = '';
-};
 
-let command = '';
-
-if (platform.includes('mac')) {
-  command = 'ke-mac';
-} else if (platform.includes('win')) {
-  command = 'ke-win.exe';
-} else if (platform.includes('linux')) {
-  command = 'ke-linux';
-}
-
-let id = 0;
-let localhostWebsite = '';
-
-let cmd = runKikoeruServe();
-
-function runKikoeruServe() {
-  const cmd = Command.create(command, [], {
-    env: {
-      LANG: 'zh_CN.UTF-8',
-      LC_ALL: 'zh_CN.UTF-8',
-    },
-    encoding: 'raw',
-  });
-
-  let output = '';
-
-  cmd.stdout.on('data', _chunk => {
-    const chunk = new Uint8Array(_chunk);
-    const str = new TextDecoder('utf-8').decode(chunk);
-    output += str;
-    const match = str.match(/(https?:\/\/[^\s]+)/);
-    if (match && match[1]) {
-      const url = match[1];
-      outputElement.innerHTML += str.replace(/(https?:\/\/[^\s]+)/, `<a href="${url}" target="_blank">${url}</a>`);
-
-      if (url.includes('localhost')) {
-        localhostWebsite = url;
-        openNewWindowButton.disabled = false;
-      }
-    } else {
-      outputElement.innerHTML += str;
+  openNewWindowButton.onclick = () => {
+    if (localhostWebsite) {
+      invoke('open_new_window', { label: `main-${id++}`, url: localhostWebsite });
     }
-  });
+  };
+  restartServiceButton.onclick = async () => {
+    (await cmd).kill();
+    cmd = runKikoeruServe();
+    localhostWebsite = '';
+    openNewWindowButton.disabled = true;
+    outputElement.innerHTML = '';
+  };
 
-  cmd.stderr.on('data', _chunk => {
-    const chunk = new Uint8Array(_chunk);
-    const str = new TextDecoder('utf-8').decode(chunk);
-    outputElement.textContent += str;
-    output += str;
-  });
+  let command = '';
 
-  cmd.on('close', () => {});
-  cmd.on('error', () => {});
+  if (platform.includes('mac')) {
+    command = 'ke-mac';
+  } else if (platform.includes('win')) {
+    command = 'ke-win.exe';
+  } else if (platform.includes('linux')) {
+    command = 'ke-linux';
+  }
 
-  return cmd.spawn();
+  let id = 0;
+  let localhostWebsite = '';
+
+  let cmd = runKikoeruServe();
+
+  function runKikoeruServe() {
+    try {
+      const cmd = Command.create(command, [], {
+        env: {
+          LANG: 'zh_CN.UTF-8',
+          LC_ALL: 'zh_CN.UTF-8',
+        },
+        encoding: 'raw',
+      });
+
+      let output = '';
+
+      cmd.stdout.on('data', _chunk => {
+        const chunk = new Uint8Array(_chunk);
+        const str = new TextDecoder('utf-8').decode(chunk);
+        output += str;
+        const match = str.match(/(https?:\/\/[^\s]+)/);
+        if (match && match[1]) {
+          const url = match[1];
+          outputElement.innerHTML += str.replace(/(https?:\/\/[^\s]+)/, `<a href="${url}" target="_blank">${url}</a>`);
+
+          if (url.includes('localhost')) {
+            localhostWebsite = url;
+            openNewWindowButton.disabled = false;
+          }
+        } else {
+          outputElement.innerHTML += str;
+        }
+      });
+
+      cmd.stderr.on('data', _chunk => {
+        const chunk = new Uint8Array(_chunk);
+        const str = new TextDecoder('utf-8').decode(chunk);
+        outputElement.textContent += str;
+        output += str;
+      });
+
+      cmd.on('close', () => {
+        message('kikoeru-express exited:\n' + output);
+      });
+      cmd.on('error', () => {
+        message('kikoeru-express error:\n' + output);
+      });
+
+      return cmd.spawn();
+    } catch (error) {
+      console.error(error);
+      message(error.toString());
+    }
+  }
+} catch (e) {
+  message(e.toString());
 }
