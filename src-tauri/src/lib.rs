171fcfccbaa4634fc72dce2b1e2aa401dd8d83da
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2025-08-28 10:54:12 +0800

    style: 调整 setup 方法调用位置 - 因为 setup 方法在一堆 plugin 里不好看

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,14 +19,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
-        .setup(|app| {
-            #[cfg(debug_assertions)]
-            {
-                let window = app.get_webview_window("main").unwrap();
-                window.open_devtools();
-            }
-            Ok(())
-        })
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
@@ -39,6 +31,14 @@ pub fn run() {
                 ])
                 .build(),
         )
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![open_new_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
